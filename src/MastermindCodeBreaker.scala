67f9136233be72390a774bec3f55f4e9ae7789d5
maxp-dlg <ian.maxwell@directlinegroup.co.uk>
2020-01-17 15:56:19 +0000

    Updating return type to integer count rather then string list

--- src/MastermindCodeBreaker.scala
+++ src/MastermindCodeBreaker.scala
@@ -1,5 +1,5 @@
 class MastermindCodeBreaker {
 
-  def breakCode(code: List[Int], guess: List[Int]): List[String] =
-    code.zip(guess).filter(x => x._1 == x._2).map(_ => "P")
+  def breakCode(code: List[Int], guess: List[Int]): Int =
+    code.zip(guess).filter(x => x._1 == x._2).map(_ => "P").length
 }
--- test/MastermindCodeBreakerTest.scala
+++ test/MastermindCodeBreakerTest.scala
@@ -4,16 +4,16 @@ class MastermindCodeBreakerTest extends AnyFlatSpec {
 
   "Mastermind" must "score guess with no matches" in {
     val mind = new MastermindCodeBreaker()
-    assert(mind.breakCode(List(0,0,0,0), List(1,1,1,1)) === List())
+    assert(mind.breakCode(List(0,0,0,0), List(1,1,1,1)) === 0)
   }
 
   "Mastermind" must "score guess with one position match" in {
     val mind = new MastermindCodeBreaker()
-    assert(mind.breakCode(List(0,0,0,0), List(0,1,1,1)) === List("P"))
+    assert(mind.breakCode(List(0,0,0,0), List(0,1,1,1)) === 1)
   }
 
   "Mastermind" must "score guess with two position matches " in {
     val mind = new MastermindCodeBreaker()
-    assert(mind.breakCode(List(0,0,0,0), List(0,1,1,0)) === List("P", "P"))
+    assert(mind.breakCode(List(0,0,0,0), List(0,1,1,0)) === 2)
   }
 }
