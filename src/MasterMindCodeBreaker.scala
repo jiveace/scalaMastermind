a2841a1ea2f6a6db879b91f25d3a1cb45399cd53
maxp-dlg <ian.maxwell@directlinegroup.co.uk>
2020-01-20 10:59:54 +0000

    Testing and implementing dec -> base 6 conversion

--- src/MasterMindCodeBreaker.scala
+++ src/MasterMindCodeBreaker.scala
@@ -1,5 +1,13 @@
 class MasterMindCodeBreaker() {
+  def numberToGuess(n: Int): List[Int] =
+    List((n / 216) % 6, (n / 36) % 6, (n / 6) % 6, n % 6)
 
-  def guessToNumber(guess: List[Int]) : Int =
-    guess.reduceLeft(_*6 + _)
+  def breakCode(value: List[Nothing]): List[Int] =
+    List(0, 0, 0, 1)
+
+  def incrementGuess(value: List[Int]): List[Int] = ???
+
+
+  def guessToNumber(guess: List[Int]): Int =
+    guess.reduceLeft(_ * 6 + _)
 }
--- test/MastermindCodeBreakerTest.scala
+++ test/MastermindCodeBreakerTest.scala
@@ -1,16 +1,34 @@
 import org.scalatest.flatspec.AnyFlatSpec
 
 class MastermindCodeBreakerTest extends AnyFlatSpec {
+  val mind = new MasterMindCodeBreaker()
 
   "guessToNumber" must "translate base 6 numbers to decimal" in {
-    val mind = new MasterMindCodeBreaker()
-    assert(mind.guessToNumber(List(0,0,0,0)) === 0)
-    assert(mind.guessToNumber(List(0,0,0,1)) === 1)
-    assert(mind.guessToNumber(List(0,0,1,0)) === 6)
-    assert(mind.guessToNumber(List(0,0,1,1)) === 7)
-    assert(mind.guessToNumber(List(0,1,1,1)) === 43)
-    assert(mind.guessToNumber(List(1,1,1,1)) === 259)
-    assert(mind.guessToNumber(List(5,5,5,5)) === 1295)
-    assert(mind.guessToNumber(List(5,5,4,1,5,5)) === 46295)
+    assert(mind.guessToNumber(List(0, 0, 0, 0)) === 0)
+    assert(mind.guessToNumber(List(0, 0, 0, 1)) === 1)
+    assert(mind.guessToNumber(List(0, 0, 1, 0)) === 6)
+    assert(mind.guessToNumber(List(0, 0, 1, 1)) === 7)
+    assert(mind.guessToNumber(List(0, 1, 1, 1)) === 43)
+    assert(mind.guessToNumber(List(1, 1, 1, 1)) === 259)
+    assert(mind.guessToNumber(List(5, 5, 5, 5)) === 1295)
+    assert(mind.guessToNumber(List(5, 5, 4, 1, 5, 5)) === 46295)
+  }
+
+  "guessToNumber" must "translate decimal to base 6 number" in {
+    assert(mind.numberToGuess(0) === List(0, 0, 0, 0))
+    assert(mind.numberToGuess(1) === List(0, 0, 0, 1))
+    assert(mind.numberToGuess(6) === List(0, 0, 1, 0))
+    assert(mind.numberToGuess(7) === List(0, 0, 1, 1))
+    assert(mind.numberToGuess(43) === List(0, 1, 1, 1))
+    assert(mind.numberToGuess(259) === List(1, 1, 1, 1))
+    assert(mind.numberToGuess(1295) === List(5, 5, 5, 5))
+  }
+
+  ignore must "increment a guess to represent the next base 6 number" in {
+    assert(mind.incrementGuess(List(0, 0, 0, 0)) === List(0, 0, 0, 1))
+  }
+
+  "breakCode" must "initialise with a base 6 0" in {
+    assert(mind.breakCode(List()) === List(0, 0, 0, 1))
   }
 }
