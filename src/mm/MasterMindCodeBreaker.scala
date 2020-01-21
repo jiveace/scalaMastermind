b6c8063b901f709cc8f09dea95f841ac059a443e
maxp-dlg <ian.maxwell@directlinegroup.co.uk>
2020-01-21 10:35:46 +0000

    Test exposing infinite loop, and throwing error to short circuit it

--- src/mm/MasterMindCodeBreaker.scala
+++ src/mm/MasterMindCodeBreaker.scala
@@ -8,7 +8,8 @@ class MasterMindCodeBreaker() {
     val mind = new MastermindCodeMaker
 
     def helper(guess: List[Int]): List[Int] =
-      if (mind.score(code = guess, guess = history.head._1) == history.head._2) guess
+      if(guess == List(0,0,0,0) ) throw new IllegalStateException("guess has looped around to (0,0,0,0")
+      else if (mind.score(code = guess, guess = history.head._1) == history.head._2) guess
       else helper(incrementGuess(guess))
 
 
