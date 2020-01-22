6d096943246d3b2f985e9f760421bf36de2107e1
maxp-dlg <ian.maxwell@directlinegroup.co.uk>
2020-01-22 11:36:36 +0000

    Stats

--- src/mm/AutoPlay.scala
+++ src/mm/AutoPlay.scala
@@ -14,6 +14,27 @@ class AutoPlay(maker: MastermindCodeMaker, breaker: MasterMindCodeBreaker, codes
 
     helper(List(), null, 1)
   }
+
+  def square(n: Int) = n * n
+
+  def mean(n: List[Int]) =
+    n.reduce(_ + _) / n.size
+
+  def median(n: List[Int]) =
+    n(n.size / 2)
+
+  def histogram(n: List[Int]): List[(Int)] =
+    n.groupBy(x => x).toList.map(x => x._2.size)
+
+  def expectedTurns(n: Int) = {
+    val scores = (1 to n).map(_ => play()).sortWith(_ < _).toList
+    Map(("mean", mean(scores)),
+      ("min", scores.head),
+      ("max", scores(scores.length - 1)),
+      ("median", median(scores)),
+      ("hist", histogram(scores)),
+    )
+  }
 }
 
 
