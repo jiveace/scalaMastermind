bd7739af42db630e76852c56fc18cb91f42b0e5f
maxp-dlg <ian.maxwell@directlinegroup.co.uk>
2020-01-22 11:52:56 +0000

    Pulling recursive helper method out so it can be reused accross all breakCode implementations

--- src/mm/MasterMindCodeBreaker.scala
+++ src/mm/MasterMindCodeBreaker.scala
@@ -1,22 +1,22 @@
 package mm
 
 class MasterMindCodeBreaker() {
+  val mind = new MastermindCodeMaker
+
   def numberToGuess(n: Int): List[Int] =
     List((n / 216) % 6, (n / 36) % 6, (n / 6) % 6, n % 6)
 
+  def nextGuess(guess: List[Int], history: List[(List[Int], (Int, Int))]): List[Int] =
+    if (guess == List(0, 0, 0, 0)) throw new IllegalStateException("guess has looped around to (0,0,0,0)")
+    else if (history.map(x => mind.score(code = guess, guess = x._1) == x._2).count(!_) == 0) guess
+    else {
+      nextGuess(incrementGuess(guess), history)
+    }
+
   def breakCodeSeq(lastGuess: List[Int], history: List[(List[Int], (Int, Int))]): List[Int] = {
-    val mind = new MastermindCodeMaker
-
-    def helper(guess: List[Int]): List[Int] =
-      if (guess == List(0, 0, 0, 0)) throw new IllegalStateException("guess has looped around to (0,0,0,0)")
-      else if (history.map(x => mind.score(code = guess, guess = x._1) == x._2).count(!_) == 0) guess
-      else {
-        helper(incrementGuess(guess))
-      }
-
     if (lastGuess == null) List(0, 0, 0, 0)
-    else if (lastGuess == List(0, 0, 0, 0)) helper(incrementGuess(lastGuess))
-    else helper(lastGuess)
+    else if (lastGuess == List(0, 0, 0, 0)) nextGuess(incrementGuess(lastGuess), history)
+    else nextGuess(lastGuess, history)
   }
 
   def incrementGuess(value: List[Int]): List[Int] =
