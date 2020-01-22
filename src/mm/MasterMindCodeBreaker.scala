07f7aa1fab66754586e9886821d0edc12a7eb316
maxp-dlg <ian.maxwell@directlinegroup.co.uk>
2020-01-22 11:49:28 +0000

    Minor refactor to prepare for 3x2

--- src/mm/MasterMindCodeBreaker.scala
+++ src/mm/MasterMindCodeBreaker.scala
@@ -19,21 +19,6 @@ class MasterMindCodeBreaker() {
     else helper(lastGuess)
   }
 
-  def breakCode3x2(lastGuess: List[Int], history: List[(List[Int], (Int, Int))]): List[Int] = {
-    val mind = new MastermindCodeMaker
-
-    def helper(guess: List[Int]): List[Int] =
-      if (guess == List(0, 0, 0, 0)) throw new IllegalStateException("guess has looped around to (0,0,0,0)")
-      else if (history.map(x => mind.score(code = guess, guess = x._1) == x._2).count(!_) == 0) guess
-      else {
-        helper(incrementGuess(guess))
-      }
-
-    if (lastGuess == null) List(0, 0, 0, 0)
-    else if (lastGuess == List(0, 0, 0, 0)) helper(incrementGuess(lastGuess))
-    else helper(lastGuess)
-  }
-
   def incrementGuess(value: List[Int]): List[Int] =
     numberToGuess(guessToNumber(value) + 1)
 
