8a59322eed75a370a37fdaf763b0b4e4b613aebb
maxp-dlg <ian.maxwell@directlinegroup.co.uk>
2020-01-22 11:56:11 +0000

    Updating loop condition

--- src/mm/MasterMindCodeBreaker.scala
+++ src/mm/MasterMindCodeBreaker.scala
@@ -7,7 +7,7 @@ class MasterMindCodeBreaker() {
     List((n / 216) % 6, (n / 36) % 6, (n / 6) % 6, n % 6)
 
   def nextGuess(guess: List[Int], history: List[(List[Int], (Int, Int))]): List[Int] =
-    if (guess == List(0, 0, 0, 0) && history.size > 0) throw new IllegalStateException("guess has looped around to (0,0,0,0)")
+    if (guess == List(0, 0, 0, 0) && history.size > 1) throw new IllegalStateException("guess has looped around to (0,0,0,0)")
     else if (history.map(x => mind.score(code = guess, guess = x._1) == x._2).count(!_) == 0) guess
     else {
       nextGuess(incrementGuess(guess), history)
@@ -15,7 +15,6 @@ class MasterMindCodeBreaker() {
 
   def breakCodeSeq(lastGuess: List[Int], history: List[(List[Int], (Int, Int))]): List[Int] = {
     if (lastGuess == null) List(0, 0, 0, 0)
-    else if (lastGuess == List(0, 0, 0, 0)) nextGuess(incrementGuess(lastGuess), history)
     else nextGuess(lastGuess, history)
   }
 
